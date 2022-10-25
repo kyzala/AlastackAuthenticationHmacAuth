0eea74d992a58b389772fb78b31ed8e167bfec88
kyzala <10436651+kyzala@users.noreply.github.com>
2022-10-25 20:54:25 +0800

    Add basic sample

--- samples/BasicSample/Program.cs
+++ samples/BasicSample/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Alastack.Authentication.Hawk;
+using System.Net.Security;
+
+// Create an SocketsHttpHandler object
+var handler = new SocketsHttpHandler
+{
+    ConnectTimeout = TimeSpan.FromSeconds(10),
+    PooledConnectionLifetime = TimeSpan.FromSeconds(1000),
+    SslOptions = new SslClientAuthenticationOptions()
+    {
+        RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true,
+    },
+    UseCookies = false
+};
+
+// Create a HawkDelegatingHandler object
+var authHandler = new HawkDelegatingHandler(new HawkSettings { AuthId = "id123", AuthKey = "3@uo45er?" })
+{
+    InnerHandler = handler
+};
+
+// Create an HttpClient object
+var client = new HttpClient(authHandler, disposeHandler: false)
+{
+    BaseAddress = new Uri("https://localhost:5001/")
+};
+
+// Call asynchronous network methods in a try/catch block to handle exceptions
+try
+{
+    var response = await client.GetAsync("/api/TodoItems");
+
+    response.EnsureSuccessStatusCode();
+
+    string responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("\nException Caught!");
+    Console.WriteLine("Message :{0} ", e.Message);
+}
+
+// Need to call dispose on the HttpClient and DelegatingHandlers objects
+// when done using them, so the app doesn't leak resources
+handler.Dispose();
+authHandler.Dispose();
+client.Dispose();
