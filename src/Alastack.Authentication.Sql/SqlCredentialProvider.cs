c78aff942dc510282daacbeab22a0281f8966e48
kyzala <10436651+kyzala@users.noreply.github.com>
2022-10-29 22:00:53 +0800

    Add SqlCredentialProvider

---
+++ src/Alastack.Authentication.Sql/SqlCredentialProvider.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System.Data;
+
+namespace Alastack.Authentication.Sql
+{
+    public class SqlCredentialProvider<TCredential, TConnection> : ICredentialProvider<TCredential> where TConnection : IDbConnection, new()
+    {
+        public string _connectionString { get; }
+        private readonly string _sql;
+
+        public SqlCredentialProvider(string connectionString, string sql)
+        {
+            _connectionString = connectionString;
+            _sql = sql;
+        }
+
+        public virtual async Task<TCredential?> GetCredentialAsync(string id)
+        {
+            using var connection = new TConnection { ConnectionString = _connectionString };
+            return await connection.QueryFirstOrDefaultAsync<TCredential>(_sql, new { Id = id });
+        }
+    }
+}
