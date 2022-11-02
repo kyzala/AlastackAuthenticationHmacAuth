f0052497570403e700c576bcedc09187747556fd
kyzala <10436651+kyzala@users.noreply.github.com>
2022-11-02 21:03:41 +0800

    Create SqlCredentialProviderSettings.cs

---
+++ src/Alastack.Authentication.Sql/SqlCredentialProviderSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Alastack.Authentication.Sql
+{
+    public class SqlCredentialProviderSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string QuerySql { get; set; }
+
+        public string SqlType { get; set; }
+    }
+}
