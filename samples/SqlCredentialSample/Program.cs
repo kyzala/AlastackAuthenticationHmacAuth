3a5e51608580608c454022ef3955b2225b238c5a
kyzala <10436651+kyzala@users.noreply.github.com>
2022-11-03 22:05:04 +0800

    Update Program.cs

--- samples/SqlCredentialSample/Program.cs
+++ samples/SqlCredentialSample/Program.cs
@@ -9,16 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddAuthentication()
 .AddHawk(options =>
 {
-    options.ForwardIndex = 4;
     options.EnableServerAuthorization = true;
     options.CredentialProvider = new SqlCredentialProvider<HawkCredential, SqlConnection>(
         builder.Configuration.GetConnectionString("CredentialDbConnection"),
         "SELECT AuthId,AuthKey,HmacAlgorithm,HashAlgorithm,User,EnableServerAuthorization,IncludeResponsePayloadHash FROM HawkCredentials WHERE AuthId = @Id");
-    options.Events.OnSetSpecificData = context => { context.Data = "specific data"; return Task.CompletedTask; };
 })
 .AddHmac(options =>
 {
-    options.ForwardIndex = 4;
     options.CredentialProvider = new SqlCredentialProvider<HmacCredential, SqlConnection>(
         builder.Configuration.GetConnectionString("CredentialDbConnection"),
         "SELECT AppId,AppKey,HmacAlgorithm,HashAlgorithm FROM HmacCredentials WHERE AppId = @Id");
