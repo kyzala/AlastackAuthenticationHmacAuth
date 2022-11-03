d3ba62fa76a7c501c954b71753dc602066fe0ab8
kyzala <10436651+kyzala@users.noreply.github.com>
2022-11-03 21:04:19 +0800

    Add MongoDB CredentialProvider

---
+++ src/Alastack.Authentication.MongoDB/MongoDBCredentialProvider.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using System.Net;
+
+namespace Alastack.Authentication.MongoDB
+{
+    public class MongoDBCredentialProvider<TCredential> : ICredentialProvider<TCredential>
+    {
+        public MongoDBCredentialProviderSettings Settings { get; set; } = default!;
+
+        public MongoDBCredentialProvider(MongoDBCredentialProviderSettings settings) => Settings = settings ?? throw new ArgumentNullException(nameof(Settings));
+
+        public virtual async Task<TCredential?> GetCredentialAsync(string id)
+        {
+            var client = new MongoClient(Settings.ConnectionString);
+            var database = client.GetDatabase(Settings.DatabaseName);
+            var collection = database.GetCollection<BsonDocument>(Settings.CollectionName);
+            var documents = await collection.FindAsync(new BsonDocument(Settings.Id, id));
+            var document = await documents.SingleOrDefaultAsync();
+            var credential = (TCredential?)BsonTypeMapper.MapToDotNetValue(document);
+            return credential;
+        }
+    }
+}
---
+++ src/Alastack.Authentication.MongoDB/MongoDBCredentialProviderSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alastack.Authentication.MongoDB
+{
+    public class MongoDBCredentialProviderSettings
+    {
+        public string ConnectionString { get; set; } = default!;
+
+        public string DatabaseName { get; set; } = default!;
+
+        public string CollectionName { get; set; } = default!;
+
+        public string Id { get; set; } = default!;
+    }
+}
