72b9af5ccb33ad9a6bdd5a38c01daf5ed04a39e9
kyzala <10436651+kyzala@users.noreply.github.com>
2022-11-06 20:29:06 +0800

    Update MongoDB Sample

--- samples/MongoDBCredentialSample/Program.cs
+++ samples/MongoDBCredentialSample/Program.cs
@@ -2,19 +2,30 @@ using Alastack.Authentication.Hawk.AspNetCore;
 using Alastack.Authentication.Hmac.AspNetCore;
 using Alastack.Authentication.MongoDB;
 
+// mongodb://localhost:27017?appname=authsample&directConnection=true&serverSelectionTimeoutMS=2000
+// mongodb://authreader:Password@localhost:27017?appname=authsample&directConnection=true&serverSelectionTimeoutMS=2000&authSource=credentialdb
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMemoryCache();
 builder.Services.AddControllers();
 builder.Services.AddAuthentication()
 .AddHawk(options =>
 {
-    options.CredentialProvider = new MongoDBCredentialProvider<HawkCredential>("mongodb://localhost:27017", "credentials", "hawk", "authId");
+    options.CredentialProvider = new MongoDBCredentialProvider<HawkCredential>(
+        "mongodb://authreader:Password@localhost:27017?appname=authsample&directConnection=true&serverSelectionTimeoutMS=2000&authSource=credentialdb",
+        "credentialdb",
+        "hawk",
+        "authId");
 })
 .AddHmac(options =>
 {
-    options.CredentialProvider = new MongoDBCredentialProvider<HmacCredential>("mongodb://localhost:27017", "credentials", "hmac", "appId");
+    options.CredentialProvider = new MongoDBCredentialProvider<HmacCredential>(
+        "mongodb://authreader:Password@localhost:27017?appname=authsample&directConnection=true&serverSelectionTimeoutMS=2000&authSource=credentialdb",
+        "credentialdb", 
+        "hmac",
+        "appId");
 });
-
+//mongodb://authUser:Test2022@localhost:27017/credentials
 var app = builder.Build();
 
 app.UseAuthentication();
---
+++ samples/MongoDBCredentialSample/init_data.js
@@ -0,0 +1,69 @@
+use('credentialdb')
+
+// -------------------------------------
+
+//db.hmac.drop()
+db.hawk.insert(
+    {
+        'authId': 'id123',
+        'authKey': '3@uo45er?',
+        'hmacAlgorithm': 'HMACSHA256',
+        'hashAlgorithm': 'SHA256',
+        'user': null,
+        'enableServerAuthorization': true,
+        'includeResponsePayloadHash': true,
+        'createdTime': new Date('2022-11-05T01:00:00Z')
+    }
+)
+
+db.hawk.createIndex({authId: 1}, {unique: true})
+
+//db.hawk.find({ 'authId': 'id123' })
+
+
+// -------------------------------------
+
+//db.hmac.drop()
+db.hmac.insert(
+    {
+        'appId': 'id123',
+        'appKey': '3@uo45er?',
+        'hmacAlgorithm': 'HMACSHA256',
+        'hashAlgorithm': 'SHA256',
+        'createdTime': new Date('2022-11-05T01:00:00Z')
+    }
+)
+
+db.hmac.createIndex({appId: 1}, {unique: true})
+
+//db.hmac.find({ 'appId': 'id123' })
+
+
+
+/*
+use admin
+
+db.system.users.remove({user: "myUserAdmin"})
+
+db.createUser(
+    {
+        user: "myUserAdmin",
+        pwd: "Password",
+        roles: [
+            { role: "userAdminAnyDatabase", db: "admin" },
+            { role: "readWriteAnyDatabase", db: "admin" }
+        ]
+    }
+)
+
+use credentialdb
+db.createUser(
+    {
+        user: "authreader",
+        pwd: "Password",
+        roles: [
+            { role: "read", db: "credentialdb" },
+        ]
+    }
+)
+*/
