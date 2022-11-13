5981e536bb126eaeddac137b45230ed3868f928a
kyzala <10436651+kyzala@users.noreply.github.com>
2022-11-13 19:26:58 +0800

    Add LiteDBCredentialProvider.

---
+++ src/Alastack.Authentication.LiteDB/LiteDBCredentialProviderSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace Alastack.Authentication.LiteDB
+{
+    /// <summary>
+    /// LiteDB CredentialProvider settings.
+    /// </summary>
+    public class LiteDBCredentialProviderSettings
+    {
+        /// <summary>
+        /// Credential database connection string.
+        /// </summary>
+        public string ConnectionString { get; set; } = default!;
+                
+        /// <summary>
+        /// Credential collection name.
+        /// </summary>
+        public string CollectionName { get; set; } = default!;
+
+        /// <summary>
+        /// Credential id field name.
+        /// </summary>
+        public string KeyName { get; set; } = default!;
+    }
+}
