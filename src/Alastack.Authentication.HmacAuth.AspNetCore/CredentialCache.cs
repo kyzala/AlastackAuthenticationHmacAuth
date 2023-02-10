91ebcb0484a262eab92a94d15429e1729968386e
kyzala <10436651+kyzala@users.noreply.github.com>
2023-02-10 21:29:30 +0800

    Add credential cache class.

---
+++ src/Alastack.Authentication.HmacAuth.AspNetCore/CredentialCache.cs
@@ -0,0 +1,35 @@
+﻿using Alastack.Authentication.AspNetCore;
+
+namespace Alastack.Authentication.HmacAuth.AspNetCore
+{
+    /// <summary>
+    /// The common implementation of <see cref="ICredentialCache{TCredential}"/>.
+    /// </summary>
+    /// <typeparam name="TCredential">A credential type.</typeparam>
+    public class CredentialCache<TCredential> : ICredentialCache<TCredential>
+    {
+        private readonly IDataCache _dataCache;
+
+        
+        /// <summary>
+        /// Initializes a new instance of <see cref="CredentialCache{TCredential}"/>.
+        /// </summary>
+        /// <param name="dataCache"><see cref="IDataCache"/></param>
+        public CredentialCache(IDataCache dataCache) 
+        {            
+            _dataCache = dataCache;
+        }
+
+        /// <inheritdoc />
+        public async Task<TCredential?> GetCredentialAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task SetCredentialAsync(string key, TCredential credential, long cacheTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
