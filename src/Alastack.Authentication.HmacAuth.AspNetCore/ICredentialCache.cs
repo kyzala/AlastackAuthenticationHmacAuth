465ff3b43e7472cd913c12524141074abeaa6b7e
kyzala <10436651+kyzala@users.noreply.github.com>
2023-02-11 21:20:23 +0800

    Add ICredentialCache.

--- src/Alastack.Authentication.HmacAuth.AspNetCore/ICredentialCache.cs
+++ src/Alastack.Authentication.HmacAuth.AspNetCore/ICredentialCache.cs
@@ -12,13 +12,13 @@ namespace Alastack.Authentication.HmacAuth.AspNetCore
     /// <typeparam name="TCredential">a credential type.</typeparam>
     public interface ICredentialCache<TCredential>
     {
-
         /// <summary>
         /// Gets a credential with the given id.
         /// </summary>
         /// <param name="key">credential cache key</param>
+        /// <param name="token">Optional. A <see cref="CancellationToken" /> to cancel the operation.</param>
         /// <returns>The task object representing the asynchronous operation, containing the located value or null.</returns>
-        Task<TCredential?> GetCredentialAsync(string key);
+        Task<TCredential?> GetCredentialAsync(string key, CancellationToken token = default);
 
         /// <summary>
         /// Sets the credential with the given key.
@@ -26,7 +26,8 @@ namespace Alastack.Authentication.HmacAuth.AspNetCore
         /// <param name="key">credential cache key</param>
         /// <param name="credential">credential instance.</param>
         /// <param name="cacheTime">credential cache time.</param>
+        /// <param name="token">Optional. A <see cref="CancellationToken" /> to cancel the operation.</param>
         /// <returns>Task that represents the asynchronous operation.</returns>
-        Task SetCredentialAsync(string key, TCredential credential, long cacheTime);
+        Task SetCredentialAsync(string key, TCredential credential, long cacheTime, CancellationToken token = default);
     }
 }
